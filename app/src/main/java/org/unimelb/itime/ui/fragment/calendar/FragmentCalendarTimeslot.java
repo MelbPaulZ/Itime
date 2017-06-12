cd6e0a308521b5b4808e0d07e0dd5a2ce02ab327
MelbPaulZ <ppsunshing@gmail.com>
2017-06-12 10:34:37 +1000

    add:david

--- app/src/main/java/org/unimelb/itime/ui/fragment/calendar/FragmentCalendarTimeslot.java
+++ app/src/main/java/org/unimelb/itime/ui/fragment/calendar/FragmentCalendarTimeslot.java
@@ -10,11 +10,15 @@ import android.view.ViewGroup;
 import org.unimelb.itime.R;
 import org.unimelb.itime.base.ItimeBaseFragment;
 import org.unimelb.itime.base.ToolbarInterface;
+import org.unimelb.itime.bean.TimeSlot;
 import org.unimelb.itime.databinding.FragmentCalendarTimeslotBinding;
 import org.unimelb.itime.manager.EventManager;
 import org.unimelb.itime.ui.mvpview.calendar.CalendarMvpView;
 import org.unimelb.itime.ui.presenter.CalendarPresenter;
 import org.unimelb.itime.ui.viewmodel.ToolbarViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import david.itimecalendar.calendar.mudules.monthview.DayViewBody;
 import david.itimecalendar.calendar.mudules.weekview.TimeSlotView;
@@ -77,14 +81,32 @@ public class FragmentCalendarTimeslot extends ItimeBaseFragment<CalendarMvpView,
         //ITimeEventPackageInterface is composed by two parts:
         //  1: regular events. 2: repeated events.
         timeSlotView.setEventPackage(eventManager.getEventsPackage());
+        timeSlotView.enableTimeSlot();
         timeSlotView.setOnTimeSlotListener(new TimeslotViewBodyListener());
+        timeSlotView.setOnTimeslotDurationChangedListener(new TimeSlotView.OnTimeslotDurationChangedListener() {
+            @Override
+            public void onTimeslotDurationChanged(long duration) {
+                if (duration == -1){
+                    //means all day
+                    timeSlotView.setViewMode(DayViewBody.Mode.ALL_DAY);
+                }else{
+                    timeSlotView.setViewMode(DayViewBody.Mode.REGULAR);
+                    timeSlotView.setTimeslotDuration(duration,false);
+                }
+            }
+        });
+        timeSlotView.setTimeslotDurationItems(initList());
+
     }
 
     private class TimeslotViewBodyListener implements DayViewBody.OnViewBodyTimeSlotListener{
-
         @Override
-        public void onAllDayRcdTimeslotClick(long l) {
-
+        public void onAllDayRcdTimeslotClick(long dayBeginMilliseconds) {
+            TimeSlot newSlot = new TimeSlot();
+            newSlot.setIsAllDay(true);
+            //ensure set the start time correctly, otherwise it cannot be shown
+            newSlot.setStartTime(dayBeginMilliseconds);
+            timeSlotView.addTimeSlot(newSlot);
         }
 
         @Override
@@ -94,17 +116,25 @@ public class FragmentCalendarTimeslot extends ItimeBaseFragment<CalendarMvpView,
 
         @Override
         public void onTimeSlotCreate(DraggableTimeSlotView draggableTimeSlotView) {
-
+            TimeSlot newSlot = new TimeSlot();
+            newSlot.setStartTime(draggableTimeSlotView.getNewStartTime());
+            newSlot.setEndTime(draggableTimeSlotView.getNewEndTime());
+            timeSlotView.addTimeSlot(newSlot);
         }
 
         @Override
         public void onTimeSlotClick(DraggableTimeSlotView draggableTimeSlotView) {
-
+//                timeslotView.reloadTimeSlots(false);
         }
 
-        @Override
-        public void onRcdTimeSlotClick(RecommendedSlotView recommendedSlotView) {
 
+        @Override
+        public void onRcdTimeSlotClick(RecommendedSlotView v) {
+            v.getWrapper().setSelected(true);
+            TimeSlot newSlot = new TimeSlot();
+            newSlot.setStartTime(v.getWrapper().getTimeSlot().getStartTime());
+            newSlot.setEndTime(v.getWrapper().getTimeSlot().getEndTime());
+            timeSlotView.addTimeSlot(newSlot);
         }
 
         @Override
@@ -113,23 +143,45 @@ public class FragmentCalendarTimeslot extends ItimeBaseFragment<CalendarMvpView,
         }
 
         @Override
-        public void onTimeSlotDragging(DraggableTimeSlotView draggableTimeSlotView, MyCalendar myCalendar, int i, int i1) {
+        public void onTimeSlotDragging(DraggableTimeSlotView draggableTimeSlotView, MyCalendar curRealCal, int x, int y) {
 
         }
 
         @Override
-        public void onTimeSlotDragDrop(DraggableTimeSlotView draggableTimeSlotView, long l, long l1) {
-
+        public void onTimeSlotDragDrop(DraggableTimeSlotView draggableTimeSlotView, long startTime, long endTime) {
+            draggableTimeSlotView.getTimeslot().setStartTime(startTime);
+            draggableTimeSlotView.getTimeslot().setEndTime(endTime);
         }
 
         @Override
         public void onTimeSlotEdit(DraggableTimeSlotView draggableTimeSlotView) {
-
+            draggableTimeSlotView.getTimeslot().setStartTime(draggableTimeSlotView.getNewStartTime());
+            draggableTimeSlotView.getTimeslot().setEndTime(draggableTimeSlotView.getNewEndTime());
         }
 
         @Override
         public void onTimeSlotDelete(DraggableTimeSlotView draggableTimeSlotView) {
-
+            timeSlotView.removeTimeslot(draggableTimeSlotView.getWrapper());
         }
+
+    }
+
+    private List initList(){
+        List<TimeSlotView.DurationItem> list= new ArrayList<>();
+        int target = 5;
+        for (int i = 1; i < target+1; i++) {
+            TimeSlotView.DurationItem item = new TimeSlotView.DurationItem();
+            if (i == target){
+                item.showName = "All Day";
+                item.duration = -1;
+            }else{
+                item.showName = "" + i + " hrs";
+                item.duration = i * 3600 * 1000;
+            }
+
+            list.add(item);
+        }
+
+        return list;
     }
 }
