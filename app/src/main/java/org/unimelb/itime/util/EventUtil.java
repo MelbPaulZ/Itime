570d144da3e69579e62d722933c9dc4c346e03c1
HuangQiushuo <379258670@qq.com>
2017-08-19 17:06:06 +1000

    add: edit event change invitee

--- app/src/main/java/org/unimelb/itime/util/EventUtil.java
+++ app/src/main/java/org/unimelb/itime/util/EventUtil.java
@@ -608,7 +608,7 @@ public class EventUtil extends BaseUtil{
     public static String getInviteeStatus(Event event){
         for (Invitee invitee:event.getInvitee().values()
                 ) {
-            if (invitee.getUserUid().equals(event.getHostUserUid())){
+            if (invitee.getInviteeUid().equals(event.getSelf())){
                 return invitee.getStatus();
             }
         }
